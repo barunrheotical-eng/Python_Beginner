3d2bacb1d908062ad5c2ced14791ac6896192c69
barunrheotical-eng <barunrheotical@gmail.com>
2025-08-19 14:42:44 -0400

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number : "))
+b=int(input("Enter another number : "))
+sum= a+b
+print("The sum of", a, "and", b, "is", sum)
+print("This script adds two numbers together.")
+sub=a-b
+print("The difference between", a, "and", b, "is", sub)
+print("This script also calculates the difference between two numbers.")
+multiply=a*b
+print("The product of", a, "and", b, "is", multiply)
+print("This script multiplies two numbers together.")
+div=a/b
+print("The quotient of", a, "and", b, "is", div)
+print("This script divides one number by another.")
