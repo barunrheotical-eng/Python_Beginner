be64dea8147965979f2cac8115c064b2a5afcede
barunrheotical-eng <barunrheotical@gmail.com>
2025-08-19 14:44:12 -0400

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+#takes input fm user
+first_name=input("Enter first name here pls :")
+last_name=input("Enter last name here pls : ")
+full_name=f"{first_name} {last_name}"
+print("Hello!", full_name, "Welcome to the coding world!")
+#This script greets the user with their full name.
